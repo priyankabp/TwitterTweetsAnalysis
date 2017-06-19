86e0dc30dfe64e92ed9c72543510e4ff68e02ca4
Priyanka Phapale <pbphapale@gmail.com>
2017-06-19 19:42:42 -0400

    Added Comments

--- twitterStreamingData.py
+++ twitterStreamingData.py
@@ -1,8 +1,14 @@
-# importing required methods from tweepy liabrary
+#############################################################################################################
+# Filename    : twitterStreamingData.py                                                                     #
+# Description : This script is used for reading live tweets from twitter which have the specified keywords. #
+#               The analysis of the tweets is done on the basis of the number of occurrences of the keyword #
+#               and then the graph is drawn.                                                                #
+#############################################################################################################
+
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from matplotlib import animation
+
 import logging
 import time
 import pandas as pd
@@ -10,14 +16,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-
-
+#twitter account details to access live data from twitter
 TWITTER_APP_KEY = 'eNMg4hN0KKQkAAXEQd2HtNkU5'
 TWITTER_APP_SECRET = 'dVPBcXsNtj3G3jhXtI5krSZv1bDP9UKfVgVo8PPymGUeqcuOZ6'
 TWITTER_KEY = '2314892245-oF7MigqkMVM3koh429nFNFiCVuIKKldxAMWAtC9'
 TWITTER_SECRET = 'qmhY8X6owWu22ZiDHjkucXL8nkrRhlrgqg7arukW9xeGO'
 
-
+# twitter lisntener class which reads live data from twitter and stores it in .csv file.
 class TweetsListener(StreamListener):
 
     def __init__(self, keywords):
@@ -36,7 +41,6 @@ class TweetsListener(StreamListener):
             saveTwitterData = open('twitterStreamingData.csv', "a")
             saveTwitterData.write(saveTweet)
             saveTwitterData.write('\n')
-            #self.count = self.count + 1
             saveTwitterData.close()
             return True
         except BaseException as e:
@@ -52,15 +56,20 @@ class TweetsListener(StreamListener):
     def disconnect(self):
         self.__stream.disconnect()
 
+
 if __name__ == '__main__':
+
     logging.basicConfig(level=logging.INFO)
-    # filtering twitter stream to capture data by keywords: 'datascience','python','datamining'
+
+    # filtering twitter stream to capture data by keywords: 'python','javascript','java'
     keywords = ['python', 'javascript', 'java']
     listener = TweetsListener(keywords)
 
+    # reading twitter data for 60 seconds and then performing analysis on the stored data in file.
     time.sleep(60)
     listener.disconnect()
 
+    # filtering the tweets and storing only 'created_at' and 'text' parameter of the tweet.
     f = open("twitterStreamingData.csv")
     final = csv.writer(open('tweets.csv', 'w'))
     final.writerow(['created_at', 'text'])
@@ -70,6 +79,7 @@ if __name__ == '__main__':
     tweets = pd.read_csv('tweets.csv', error_bad_lines=False)
     tweets.head()
 
+    # Counting the occurrence of each keyword in tweets.
     def get_tweets(row):
         languages = []
         if type(row['text']) is str:
@@ -86,6 +96,8 @@ if __name__ == '__main__':
 
     counts = tweets['language'].value_counts()
     del counts['']
+
+    # plotting the bar graph with the analysis of data from the tweets.
     plt.bar(range(len(counts)),counts)
     plt.title('Programming languages mostly spoken about')
     plt.xlabel('Languages')
